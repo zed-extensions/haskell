e01845bfb3a2effe232e69c544e22a85453b610c
MrSubidubi <dev@bahn.sh>
2026-06-04 12:43:33 +0200

    Format

--- languages/literate-haskell/highlights.scm
+++ languages/literate-haskell/highlights.scm
@@ -1,8 +1,10 @@
 ; Bird track marker
-(bird_line ">" @punctuation.special)
+(bird_line
+  ">" @punctuation.special)
 
 ; LaTeX delimiters
 (latex_begin) @keyword.directive
+
 (latex_end) @keyword.directive
 
 ; Highlight LaTeX comments like comments
@@ -10,6 +12,7 @@
 
 ; Markdown delimiters
 (markdown_begin) @keyword.directive
+
 (markdown_end) @keyword.directive
 
 ; Normal prose is not highlighted. Haskell code will be
