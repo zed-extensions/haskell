ad4a3a9293b715a80d2ef483f2692d0ceeca7048
jk <47693+sectore@users.noreply.github.com>
2026-06-25 08:23:52 +0200

    run Zed's `tree-sitter-query` extension
    to format / lint

--- languages/haskell/runnables.scm
+++ languages/haskell/runnables.scm
@@ -2,5 +2,6 @@
 ((apply
   function: (variable) @run
   (#any-of? @run "describe" "it")
-  argument: (literal (string) @HASKELL_TEST_NAME)) @_haskell-test
+  argument: (literal
+    (string) @HASKELL_TEST_NAME))
   (#set! tag haskell-test))
